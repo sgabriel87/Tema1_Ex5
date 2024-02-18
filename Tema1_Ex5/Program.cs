8c63fc864fe4eed89812352ddaec307ab17aa134
Gabriel <Gabriel@GABRIEL>
2024-02-18 20:01:22 +0200

    Add project files.

---
+++ Tema1_Ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Se citesc doua numere de la tastatura, x, y. Scrieti un program care va afisa cele
+doua valori in ordine crescatoare.
+• Exemplu: citim 9,0 Afisam : 0 9*/
+
+Console.WriteLine("Introduceti primul numar:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduceti al doilea numar:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+if (x > y)
+{   //valoare temporara de sotcare
+    int temp = x;
+    //x devine y
+    x = y;
+    //y devine valoare temp x
+    y = temp;
+}
+
+Console.WriteLine("Cele doua numere in ordine crescatoare:");
+Console.WriteLine(x + " " + y);
